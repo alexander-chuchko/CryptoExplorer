4bfc87d8e059e4549e5677d4b2cfb96a2f214c9b
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-01-14 16:32:28 +0200

    Added ResponceCurrency model

---
+++ CryptoExplorer/CryptoExplorer/Models/ResponceCurrency.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CryptoExplorer.Models
+{
+    public class ResponceCurrency
+    {
+        [JsonProperty("timestamp")]
+        public string? Timestamp { get; set; }
+
+        [JsonProperty("data")]
+        public IEnumerable<Currency>? Currencies { get; set; }
+    }
+}
