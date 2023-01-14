ba51fcc305b20626786686a9aa7e8bad8a6755d7
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-01-15 01:30:53 +0200

    Added ResponceMarket model

---
+++ CryptoExplorer/CryptoExplorer/Models/ResponceMarket.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace CryptoExplorer.Models
+{
+    public class ResponceMarket
+    {
+        [JsonProperty("timestamp")]
+        public string? Timestamp { get; set; }
+
+        [JsonProperty("data")]
+        public IEnumerable<Market>? Currencies { get; set; }
+    }
+}
