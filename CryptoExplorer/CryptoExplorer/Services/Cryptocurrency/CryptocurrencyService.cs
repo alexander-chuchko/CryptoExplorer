6e7dff3c9cd390def822605888a4638eced378e2
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-01-14 17:10:46 +0200

    Added the GetTopCurrenciesAsync method to the CryptocurrencyService class

--- CryptoExplorer/CryptoExplorer/Services/Cryptocurrency/CryptocurrencyService.cs
+++ CryptoExplorer/CryptoExplorer/Services/Cryptocurrency/CryptocurrencyService.cs
@@ -1,4 +1,5 @@
 ﻿using CryptoExplorer.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,7 +13,7 @@ namespace CryptoExplorer.Services.Cryptocurrency
 {
     public class CryptocurrencyService : ICryptocurrencyService
     {
-        public const string WEB_API = "markets";
+        public const string WEB_API = "assets?poloniex=Binance&limit=10"; 
         public const string BASE_URL = "https://api.coincap.io/v2/";
         public const string API_KEY = "34d7b96b-dafd-4c0f-88fb-6d588ea564ff";
         private HttpClient _client;
@@ -26,21 +27,18 @@ namespace CryptoExplorer.Services.Cryptocurrency
         }
         public async Task<IEnumerable<Currency>> GetTopCurrenciesAsync(int limit = 0)
         {
-            List<Currency> collection = null;
-
+            IEnumerable<Currency>? сurrencies = null;
+            //Helpers
             try
             {
                 HttpResponseMessage response = await _client.GetAsync(WEB_API);
-                response.EnsureSuccessStatusCode();
 
-                if (response.IsSuccessStatusCode)
+                if (response.EnsureSuccessStatusCode().IsSuccessStatusCode && response.StatusCode == HttpStatusCode.OK)
                 {
-                    if (response.StatusCode == HttpStatusCode.OK)
-                    {
-                        var responceCurrency = await response.Content.ReadAsStringAsync();
-                        //var deserializedResponceCurrency = 
-                    }
+                    var responceCurrency = await response.Content.ReadAsStringAsync();
+                    var deserializedResponceCurrency = JsonConvert.DeserializeObject<ResponceCurrency>(responceCurrency);
 
+                    сurrencies = deserializedResponceCurrency?.Currencies?.ToList();
                 }
             }
             catch (Exception ex)
@@ -48,7 +46,7 @@ namespace CryptoExplorer.Services.Cryptocurrency
                 Debug.WriteLine(ex.Message);
             }
 
-            return collection;
+            return сurrencies;
         }
     }
 }
